b534dd3bb2b9802255db23d38c10ab484b34f75b
Aazib Chaudhry <aazibch47@gmail.com>
2022-10-27 07:49:23 +0500

    Implemented functionality to add or remove items from cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
 
-    const numOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+    const { items } = cartCtx;
+
+    const numOfCartItems = items.reduce((currentNum, item) => {
         return currentNum + item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${
+        btnIsHighlighted ? classes.bump : ''
+    }`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+
+        setBtnIsHighlighted(true);
+
+        const timerRef = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timerRef);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
